b78540bb9656877d0c7cebeab43fe718b07b665a
Daniel Noyola <pepenoyola13@gmail.com>
2026-05-08 16:42:00 -0600

    feat(auth): Flujo de registro del usuario implementado

---
+++ resources/js/modules/auth/pages/Register.tsx
@@ -0,0 +1,13 @@
+import { store } from "@/actions/App/Http/Controllers/Auth/RegisterController"
+import { Form } from "@inertiajs/react"
+
+
+const Register = () => {
+  return (
+    <div>
+      Pagina de registro
+    </div>
+  )
+}
+
+export default Register
